bf0142d25e8d3014745f4ee1d58d064b1e393e03
Petr Kadlec <xkadlec6@fi.muni.cz>
2023-08-15 16:15:35 +0200

    Feat: Finished lesson 6

--- app/src/main/kotlin/app/futured/academyproject/domain/GetCulturalPlacesUseCase.kt
+++ app/src/main/kotlin/app/futured/academyproject/domain/GetCulturalPlacesUseCase.kt
@@ -13,5 +13,25 @@ class GetCulturalPlacesUseCase @Inject constructor(
     //  Implementuj metódu build, ktorá vráti zoznam miest.
     //  Využi ApiManager a namapuj CulturalPlaces na List<Place>.
     //  Mali by stačiť iba nasledujúce properties: id, longitude, latitude, name, type, note,
-    override suspend fun build(args: Unit): List<Place> = TODO("Just keep coding, just keep coding...")
+    override suspend fun build(args: Unit): List<Place> = apiManager.getCulturalPlaces().features.map {
+        val value = it.properties;
+
+        Place(
+            id = value.ogcFid,
+            longitude = it.geometry?.coordinates?.getOrNull(0),
+            latitude = it.geometry?.coordinates?.getOrNull(0),
+            name = value.name,
+            type = value.type,
+            note = value.note,
+            image1Url = value.image1Url,
+            webUrl = value.webUrl,
+            program = value.program,
+            street = value.street,
+            streetNumber = value.streetNumber,
+            email = value.email,
+            phone = value.email,
+            openFrom = value.openFrom,
+            openTo = value.openTo
+        )
+    }
 }
--- app/src/main/kotlin/app/futured/academyproject/injection/modules/NetworkModule.kt
+++ app/src/main/kotlin/app/futured/academyproject/injection/modules/NetworkModule.kt
@@ -1,13 +1,23 @@
 package app.futured.academyproject.injection.modules
 
+import app.futured.academyproject.BuildConfig
 import app.futured.academyproject.data.remote.ApiService
+import app.futured.academyproject.tools.Constants.Api.BASE_PROD_URL
+import app.futured.academyproject.tools.Constants.Api.TIMEOUT_IN_SECONDS
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.json.Json
 import okhttp3.Interceptor
+import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import timber.log.Timber
+import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 @Module
@@ -19,25 +29,45 @@ class NetworkModule {
     //  Nezabudni nastavit level na HttpLoggingInterceptor.Level.BODY a na logovanie využi Timber
     @Provides
     @Singleton
-    fun provideLoggingInterceptor(): Interceptor = TODO("Life is like a box of code. You never know what you'll write next.")
+    fun provideLoggingInterceptor(): Interceptor = HttpLoggingInterceptor {
+        message -> Timber.tag("OkHTTP").d(message)
+    }.apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    /*{
+        var httpLog = HttpLoggingInterceptor{message -> Timber.tag("OkHttp").d(message)}
+        httpLog.level = HttpLoggingInterceptor.Level.BODY
+        return httpLog;
+    }*/
 
     // TODO Krok 4:
-    //  Vytvor OkHttp vlienta HttpLoggingInterceptor a pridaj k nemu logging interceptor
+    //  Vytvor OkHttp clienta HttpLoggingInterceptor a pridaj k nemu logging interceptor
     @Provides
     @Singleton
     fun provideOkHttpClient(
         loggingInterceptor: Interceptor,
-    ): OkHttpClient = TODO("May the code be with you.")
+    ): OkHttpClient = OkHttpClient
+        .Builder()
+        .addInterceptor(loggingInterceptor)
+        .build()
 
     // TODO Krok 5:
     //  Za pomoci Retrofitu vytvor ApiService, ktorý automaticky implementuje definíciu z ApiService na reálnu implementáciu.
     //  Nezabudni pridať OkHttpClient a converterFactory `json.asConverterFactory("application/json".toMediaType()`
     //  URL nájdeš v konštatnátch ako BASE_PROD_URL
     //  Odporúča sa nastaviť aj validateEagerly(BuildConfig.DEBUG)
+    //  TODO("Channel your inner Jára Cimrman, inventor of Android and Kotlin")
     @Provides
     @Singleton
     fun provideRetrofitService(
         okHttpClient: OkHttpClient,
         json: Json,
-    ): ApiService = TODO("Channel your inner Jára Cimrman, inventor of Android and Kotlin")
+    ): ApiService = Retrofit.Builder()
+        .baseUrl(BASE_PROD_URL)
+        .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
+        .client(okHttpClient)
+        .validateEagerly(BuildConfig.DEBUG)
+        .build()
+        .create(ApiService::class.java)
 }
